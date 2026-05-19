368736426219bbbb880f3cc150d9c32d6836d4e5
Hafiz Shehryar <syedshehryarali59@gmail.com>
2026-05-19 23:13:53 +0500

    Added Day 1 Python basics code

---
+++ first_datatypes.py
@@ -0,0 +1,30 @@
+# Day 1 - Python Basics by Shehryar
+# Variables and Data Types
+
+# String
+name = "Shehryar"
+
+# Integer
+age = 18
+
+# Float
+height = 5.9
+
+# Boolean
+is_student = True
+
+# List
+hobbies = ["Gaming", "Coding", "AI"]
+
+# Dictionary
+person = {
+    "name": "Shehryar",
+    "age": 18,
+    "goal": "AI Developer"
+}
+
+# F-string printing
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"My hobbies are {hobbies}")
+print(f"My goal is {person['goal']}")
